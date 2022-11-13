42ca73d41c196267f8330a5d3e687c3c0d25982a
loidinhm31 <loidinh.git@gmail.com>
2022-11-13 21:49:49 +0700

    validate: server side form validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"fmt"
 	"github.com/loidinhm31/access-system/internal/config"
+	"github.com/loidinhm31/access-system/internal/forms"
 	"github.com/loidinhm31/access-system/internal/models"
 	"github.com/loidinhm31/access-system/internal/render"
+	"log"
 	"net/http"
 )
 
@@ -45,22 +47,54 @@ func (m *Repository) About(writer http.ResponseWriter, request *http.Request) {
 }
 
 func (m *Repository) Reservation(writer http.ResponseWriter, request *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
 
-	render.DrawTemplate(writer, request, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.DrawTemplate(writer, request, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (m *Repository) PostReservation(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: request.Form.Get("first_name"),
+		LastName:  request.Form.Get("last_name"),
+		Phone:     request.Form.Get("phone"),
+		Email:     request.Form.Get("email"),
+	}
+
+	form := forms.New(request.PostForm)
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, request)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.DrawTemplate(writer, request, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+	}
 }
 
 func (m *Repository) Generals(writer http.ResponseWriter, request *http.Request) {
-
 	render.DrawTemplate(writer, request, "generals.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Majors(writer http.ResponseWriter, request *http.Request) {
-
 	render.DrawTemplate(writer, request, "majors.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) Availability(writer http.ResponseWriter, request *http.Request) {
-
 	render.DrawTemplate(writer, request, "search-availability.page.tmpl", &models.TemplateData{})
 }
 
@@ -72,6 +106,5 @@ func (m *Repository) PostAvailability(writer http.ResponseWriter, request *http.
 }
 
 func (m *Repository) Contact(writer http.ResponseWriter, request *http.Request) {
-
 	render.DrawTemplate(writer, request, "contact.page.tmpl", &models.TemplateData{})
 }
